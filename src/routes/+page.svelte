eb0cf64d834739b13d9e2846cdf481fa6634edd5
wjojarth23 <wjojarth27@dtechhs.org>
2025-02-02 20:22:34 -0800

    image test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from 'svelte';
   import 'leaflet/dist/leaflet.css';
+  
 
   const mediaFiles = [
     'photo1.webp',
@@ -122,6 +123,12 @@
     const L = await import('leaflet');
 
     map = L.map('map').setView([0, 0], 13);
+    delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors',
